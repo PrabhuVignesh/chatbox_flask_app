8c2ea8183bcd9ce028234a15757fbe470ed0bd86
PrabhuVignesh <prabhu.vignesh1990@gmail.com>
2017-04-29 21:28:20 +0530

    change in app.py

---
+++ deploy.php
@@ -0,0 +1,49 @@
+<?php
+	/**
+	 * GIT DEPLOYMENT SCRIPT
+	 *
+	 * Used for automatically deploying websites via github or bitbucket, more deets here:
+	 *
+	 *		https://gist.github.com/1809044
+	 */
+	// The commands are 1
+	$commands = array(
+		'echo $PWD',
+		'whoami',
+		'git pull origin master',
+		'git status',
+		'git submodule sync',
+		'git submodule update',
+		'git submodule status',
+	);
+	// Run the commands for output
+	$output = '';
+	foreach($commands AS $command){
+		// Run it
+		$tmp = shell_exec($command);
+		// Output
+		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+		$output .= htmlentities(trim($tmp)) . "\n";
+	}
+	// Make it pretty for manual user access (and why not?)
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Prabhu Profile auto deploy</title>
+	<link href="img/profile1.jpg" rel="icon" type="image/jpg">
+</head>
+<body style="background-color: #425265; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<pre>
+ .  ____  .    ____________________________
+ |/      \|   |                            |
+[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Deployment                 |
+ |___==___|  / &copy; Prabhu Vignesh Kumar 2012 |
+              |____________________________|
+
+<?php echo $output; ?>
+</pre>
+</body>
+</html>
+
