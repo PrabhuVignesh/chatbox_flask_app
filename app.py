e4ef0a5825967e5397273e36211024185bffdbe6
PrabhuVignesh <prabhu.vignesh1990@gmail.com>
2017-04-29 22:19:50 +0530

    change in app

--- app.py
+++ app.py
@@ -2,14 +2,7 @@ from flask import Flask, render_template, jsonify
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
-
-
-
 app = Flask(__name__)
-class MyEncoder(JSONEncoder):
-    def default(self, o):
-        return o.__dict__  
-
 english_bot = ChatBot("English Bot")
 english_bot.set_trainer(ChatterBotCorpusTrainer)
 english_bot.train("chatterbot.corpus.english")
@@ -20,9 +13,7 @@ def home():
 
 @app.route("/<string:query>")
 def get_raw_response(query):
-	list = [
-		{'answer' : english_bot.get_response(query)}
-	]
+    list = [{'answer' : english_bot.get_response(query)}]
     return jsonify(outcome=list)
 
 
